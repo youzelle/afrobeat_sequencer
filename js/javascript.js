9b49551f33cd253b484c5485522a199424acb86a
Danni <d.youziel@gmail.com>
2018-07-19 00:44:01 +0100

    Toggle Function
    Allow user to change value of array element

--- js/javascript.js
+++ js/javascript.js
@@ -1,85 +1,90 @@
 // const drumSequencer = [true, false, false, true, true, false, false, false];
 
 // const audio = document.getElementById('soundOne');
+// let counter = 0;
 
+// let start = setInterval(playLoop, 1000);
 
-// function start() {
-// 	let counter = 0;
-// 	event.preventDefault();
-// 	setInterval(function(){ 
-// 	if (drumSequencer[counter] === true) {
-// 	audio.play().catch(function(error) {
-// 		console.log('error', error)
-// 	})}
-// 	console.log(counter);
-// 	counter++;
-// 	//loops through array
-// 		if (counter === 8) {
-// 			counter = 0;
-// 		}
-// 		//stops array if double click on screen
-// 		if (document.addEventListener().dblclick === true) {
-// 			break;
-// 		}
-		
-// }, 1000);}
-
+// function playLoop() { 
+//   if (drumSequencer[counter] === true) {
+//   audio.play(); 
+//   }
+//   console.log(counter);
+//   counter++;
+//   //loops through array
+//     if (counter === 8) {
+//       counter = 0;
+//     }
+// };  
 
 
 // function stop() {
-// 	audio.pause();
-// 	audio.currentTime = 0;
+//   clearInterval(playLoop);
 // }
 
-// //starts audio
+
+
+// //starts audio, but start must be an object
 // document.addEventListener('click', start)
 
 // //stops audio
 // document.addEventListener('dblclick', stop)
 
 
-const drumSequencer = [true, false, false, true, true, false, false, false];
+// const drumSequencer = [true, false, false, true, true, false, false, false];
+
+const drumSequencer = [ false, false, false, false, false, false, false, false];
+
+function toggle() {
+  drumSequencer[0] = !drumSequencer[0];
+  console.log(drumSequencer);
+}
 
 const audio = document.getElementById('soundOne');
 let stopped = false;
 let counter = 0;
 
 
-
-function restart(){
+function start(){
    stopped = false;
-   start();
-}
+   playAudio();
+};
 
-function start() {
+//why doesnt this work with setInterval?
+//precision of execution
+function playAudio() {
     setTimeout(function(){ 
         if (drumSequencer[counter] === true) { 
-        audio.play().catch(function(error) {
-        console.log('error', error)
-         })}
-        console.log(counter);
+        audio.play();
+        }
+        console.log(counter +':'+ Date.now());
         counter++;
         //loops through array
-            if (counter === 8) {
-                counter = 0;
-            }
-            //stops array if double click on screen
-            if (stopped==false) {
-              start();
-              counter++;
-            }
+        if (counter === 8) {
+            counter = 0;
+        }
+        //stops loop if double click on screen
+        if (stopped == false) {
+            playAudio();
+        }
     }, 1000);}
 
 
-
-function stop() {
+//doesnt stop loop immediately
+function pause() {
     audio.pause();
     audio.currentTime = 0;
     stopped = true;
-}
+};
+
+
+console.log(drumSequencer);
+
+//toggle array elements
+document.getElementById("a1").addEventListener('click', toggle);
 
 //starts audio
-document.addEventListener('click', restart)
+document.addEventListener('click', start)
 
 //stops audio
-document.addEventListener('dblclick', stop)
+document.addEventListener('dblclick', pause)
