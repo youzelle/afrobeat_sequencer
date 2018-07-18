7c6ef5998e5809a0131ec85c363de59d54a1b16c
Danni <d.youziel@gmail.com>
2018-07-18 12:32:05 +0100

    Loop Break
    Use restart to stop loop iteration on double click.

--- js/javascript.js
+++ js/javascript.js
@@ -1,27 +1,85 @@
+// const drumSequencer = [true, false, false, true, true, false, false, false];
+
+// const audio = document.getElementById('soundOne');
+
+
+// function start() {
+// 	let counter = 0;
+// 	event.preventDefault();
+// 	setInterval(function(){ 
+// 	if (drumSequencer[counter] === true) {
+// 	audio.play().catch(function(error) {
+// 		console.log('error', error)
+// 	})}
+// 	console.log(counter);
+// 	counter++;
+// 	//loops through array
+// 		if (counter === 8) {
+// 			counter = 0;
+// 		}
+// 		//stops array if double click on screen
+// 		if (document.addEventListener().dblclick === true) {
+// 			break;
+// 		}
+		
+// }, 1000);}
+
+
+
+// function stop() {
+// 	audio.pause();
+// 	audio.currentTime = 0;
+// }
+
+// //starts audio
+// document.addEventListener('click', start)
+
+// //stops audio
+// document.addEventListener('dblclick', stop)
+
+
 const drumSequencer = [true, false, false, true, true, false, false, false];
 
 const audio = document.getElementById('soundOne');
-
-// for (let i = 0; i<drumSequencer.length; i++) {
-// 	if (drumSequencer[i] === true) {
-// 		audio.play();
-// 	}
-// }
+let stopped = false;
+let counter = 0;
 
 
 
+function restart(){
+   stopped = false;
+   start();
+}
 
 function start() {
-	let counter = 0;
+    setTimeout(function(){ 
+        if (drumSequencer[counter] === true) { 
+        audio.play().catch(function(error) {
+        console.log('error', error)
+         })}
+        console.log(counter);
+        counter++;
+        //loops through array
+            if (counter === 8) {
+                counter = 0;
+            }
+            //stops array if double click on screen
+            if (stopped==false) {
+              start();
+              counter++;
+            }
+    }, 1000);}
 
-	setInterval(function(){ 
-	if (drumSequencer[counter] === true) {
-	audio.play().catch(function(error) {
-		console.log('error', error)
-	})}
-	console.log(counter);
-	counter++;
-}, 1000);}
 
 
-document.addEventListener('click', start)
+function stop() {
+    audio.pause();
+    audio.currentTime = 0;
+    stopped = true;
+}
+
+//starts audio
+document.addEventListener('click', restart)
+
+//stops audio
+document.addEventListener('dblclick', stop)
