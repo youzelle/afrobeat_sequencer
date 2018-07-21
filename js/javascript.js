83b08b5e686f899a44a529443b8d89a0eabcb8d2
Danni <d.youziel@gmail.com>
2018-07-21 18:51:00 +0100

    Counter Position
    Position of counter visible

--- js/javascript.js
+++ js/javascript.js
@@ -1,39 +1,13 @@
-// const drumSequencer = [true, false, false, true, true, false, false, false];
-
-// const audio = document.getElementById('soundOne');
-// let counter = 0;
-
-// let start = setInterval(playLoop, 1000);
-
-// function playLoop() { 
-//   if (drumSequencer[counter] === true) {
-//   audio.play(); 
-//   }
-//   console.log(counter);
-//   counter++;
-//   //loops through array
-//     if (counter === 8) {
-//       counter = 0;
-//     }
-// };  
-
-
-// function stop() {
-//   clearInterval(playLoop);
-// }
-
-
-
-// //starts audio, but start must be an object
-// document.addEventListener('click', start)
-
-// //stops audio
-// document.addEventListener('dblclick', stop)
-
-
-
 const drumSequencer = [false, false, false, false, false, false, false, false];
 
+const audio = document.getElementById('soundOne');
+let counter = 0;
+let playing;
+const numberOfItems = drumSequencer.length;
+
+//store DOM elements
+const elements = [];
+for (let i = 0; i < numberOfItems; i++) elements.push(document.getElementById("a" + i));
 
 // User selects which sound to play
 
@@ -42,7 +16,6 @@ let djembeOne = document.getElementsByClassName("a");
 function tabActive() {
     let key = this.getAttribute("data-key");
     drumSequencer[key] = !drumSequencer[key];
-    console.log(key);
     document.getElementById("a" +key).classList.toggle("isActive");
 }
 
@@ -51,53 +24,40 @@ for (let i = 0; i< djembeOne.length; i++) {
     djembeOne[i].addEventListener("click", tabActive)
 }
 
-const audio = document.getElementById('soundOne');
-let stopped = false;
-let counter = 0;
-
 
 function start() {
-   stopped = false;
-   playAudio();
-};
+    playing = setInterval(playAudio, 1000)
+}
 
-//why doesnt this work with setInterval?
-//precision of execution
+
 function playAudio() {
-    setTimeout(function(){ 
-        if (drumSequencer[counter] === true) { 
-        audio.play();
-        }
-        console.log(counter +':'+ Date.now());
-        counter++;
-        //loops through array
-        if (counter === 8) {
-            counter = 0;
-        }
-        //stops loop if double click on screen
-        if (stopped == false) {
-            playAudio();
-        }
-    }, 1000);
-    document.getElementById("a" +counter).classList.add("counterPos");
-    setTimeout(document.getElementById("a" +counter).classList.remove("counterPos"), 1000);
+    elements[counter].classList.add("counterPos");
+    if (counter > 0) {
+            elements[counter -1].classList.remove("counterPos");
+        } else {
+            elements[numberOfItems - 1].classList.remove("counterPos");
+           }
+        console.log(drumSequencer[drumSequencer[counter]]);
+    if (drumSequencer[counter] === true) { 
+       audio.play();
+     }
+    console.log(counter +':'+ Date.now());
+     counter++;
+//     //loops through array
+    if (counter === 8) {
+        counter = 0;
+     counter = counter % numberOfItems;
+    }
 };
-
 
 function pause() {
     audio.pause();
-    audio.currentTime = 0;
-    stopped = true;
+    clearInterval(playing);
 };
-
 
 console.log(drumSequencer);
 
-//see counter
-
-
 //Event Listeners
-
 
 //starts audio
 document.getElementById("start").addEventListener('click', start)
