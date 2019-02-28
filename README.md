86724d38ffa6255436370e01e75a76bcb2f198c5
Danni <d.youziel@gmail.com>
2019-02-28 18:42:19 +0000

    Refactoring

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 The music sequencer allows users to click on tabs and play drum beats on a loop. The timing of te sequence can be controlled via the bpm. The user can also record and download their drum beats.
 
 To do:
+-Refactor using MVC
 -Feature detecion for cross-browser compatibilty
--Incoporate drag and drop so user can choose instruments from a range
+
--- js/index.js
+++ js/index.js
@@ -1,34 +1,12 @@
-//MUSIC PLAYER SETUP
 
-const one = [false, false, false, false, false, false, false, false];
-const two = [false, false, false, false, false, false, false, false];
-const three = [false, false, false, false, false, false, false, false];
-const four = [false, false, false, false, false, false, false, false];
-
-const beatsMatrix = [one, two, three, four]
-
+const musicMachine = [[],[],[],[]];
+const numberOfInstr = 4;
 let counter = 0;
-let row = 0;
-let col = 0;
 let playing;
 const numberOfBeats = 8;
-const numberOfInstr = 4;
-
-//store DOM elements in JS array
-const soundLinks = [[],[],[],[]];
-
-for (let i = 0; i < numberOfBeats; i++) {
-    soundLinks[0].push(document.getElementById("a" + i));
-    soundLinks[1].push(document.getElementById("b" + i));
-    soundLinks[2].push(document.getElementById("c" + i));
-    soundLinks[3].push(document.getElementById("d" + i))
-}
-
-//SET UP AUDIO
 const audio = [];
-for (let j = 0; j < numberOfInstr; j++) {
-    audio.push(document.getElementById("audio" + j));
-}
+let chunks = [];
+
 
 try {
 
@@ -40,49 +18,63 @@ try {
     throw new Error("Web Audio API not supported.");
 }
 
-// creates a link between audio context and file
-const xylo = audioContext.createMediaElementSource(audio[0]);
-const djembeOne = audioContext.createMediaElementSource(audio[1]);
-const djembeTwo = audioContext.createMediaElementSource(audio[2]);
-const maracas = audioContext.createMediaElementSource(audio[3]);
+  //Gets stream of data from the speaker output - gives the ability to store
+  const dest = audioContext.createMediaStreamDestination();
 
-const gainNode = audioContext.createGain();
+  //new instance of MediaRecorder
+  const mediaRecorder = new MediaRecorder(dest.stream);
 
-xylo.connect(gainNode);
-djembeOne.connect(gainNode);
-djembeTwo.connect(gainNode);
-maracas.connect(gainNode);
+function setUp() {
 
-// creates link to the speaker
-gainNode.connect(audioContext.destination);
+  
 
-gainNode.gain.value = 1;
+    //store DOM elements in JS array
+    for (let i = 0; i < numberOfBeats; i++) {
+        musicMachine[0].push(document.getElementById("0" + i));
+        musicMachine[1].push(document.getElementById("1" + i));
+        musicMachine[2].push(document.getElementById("2" + i));
+        musicMachine[3].push(document.getElementById("3" + i))
+    }
 
-//Gets stream of data from the speaker output - gives the ability to store
-const dest = audioContext.createMediaStreamDestination();
-//connects sound producing part of node
-gainNode.connect(dest);
+    //get audio elements 
+    for (let j = 0; j < numberOfInstr; j++) {
+        audio.push(document.getElementById("audio" + j));
+    }
 
-//This records the stream 
-var mediaRecorder = new MediaRecorder(dest.stream);
+    //pass audio element to media context
+    const xylo = audioContext.createMediaElementSource(audio[0]);
+    const djembeOne = audioContext.createMediaElementSource(audio[1]);
+    const djembeTwo = audioContext.createMediaElementSource(audio[2]);
+    const maracas = audioContext.createMediaElementSource(audio[3]);
 
-let chunks = [];
+    const gainNode = audioContext.createGain();
 
-//when data is available an event is raised, this listens for it
-mediaRecorder.ondataavailable = function(event) {
-    chunks.push(event.data);
-}
+    gainNode.gain.value = 1;
 
-mediaRecorder.onstop = function(event) {
-    // Make blob out of our blobs, and open it.
-    const blob = new Blob(chunks, { "type" : "audio/webm;codecs=opus" });    
+    //connects sound producing part of node
+    gainNode.connect(dest);
 
-    // creates the download link
-    document.getElementById("keepBut").setAttribute("href", URL.createObjectURL(blob));
-    document.getElementById("keepBut").setAttribute("download", "drumBeat");
+    xylo.connect(gainNode).connect(audioContext.destination);
+    djembeOne.connect(gainNode).connect(audioContext.destination);
+    djembeTwo.connect(gainNode).connect(audioContext.destination);
+    maracas.connect(gainNode).connect(audioContext.destination);
 
-    chunks = [];
+    //when data is available an event is raised, this listens for it
+    mediaRecorder.ondataavailable = function(event) {
+        chunks.push(event.data);
+    }
 
+    mediaRecorder.onstop = function(event) {
+        // Make blob out of our blobs, and open it.
+        const blob = new Blob(chunks, { "type" : "audio/webm;codecs=opus" });    
+
+        // creates the download link
+        document.getElementById("keepBut").setAttribute("href", URL.createObjectURL(blob));
+        document.getElementById("keepBut").setAttribute("download", "drumBeat");
+
+        chunks = [];
+
+    }
 }
 
 //BUTTON HELPERS
@@ -119,12 +111,13 @@ function timeInterval() {
 //need to call resume() to put in running state 
 function start() {
     event.preventDefault();
-    audioContext.resume().then(() => {
+    if (audioContext.state === 'suspended') {
+        audioContext.resume();
         console.log("Playback resumed successfully");
-      });
+    }
     playing = setInterval(playAudio, timeInterval());
-    document.getElementById("pause").classList.remove("isActiveCtr");
-    document.getElementById("start").classList.add("isActiveCtr");
+    // document.getElementById("pause").classList.remove("isActiveCtr");
+    // document.getElementById("start").classList.add("isActiveCtr");
     swapButtons("start", "pause");
 
 }
@@ -132,22 +125,28 @@ function start() {
 function pause() {
     event.preventDefault();
       clearInterval(playing);
-      document.getElementById("pause").classList.add("isActiveCtr");
-      document.getElementById("start").classList.remove("isActiveCtr");
+    //   document.getElementById("pause").classList.add("isActiveCtr");
+    //   document.getElementById("start").classList.remove("isActiveCtr");
       swapButtons("pause", "start");
   }
   
 function stop() {
     event.preventDefault();
-    window.location.reload()
+    counter = 0;
+    for (let r = 0; r < numberOfInstr; r++) {
+        musicMachine[r].forEach(element => {
+            element.classList.remove("counterPos", "isActiveButt");
+    })}
+    clearInterval(playing);
+    swapButtons("pause", "start");
 }
 
 //only want to start recording if play button is active
 function startRec() {
     event.preventDefault();
     mediaRecorder.start();
-    document.getElementById("stoprec").classList.remove("isActiveCtr");
-    document.getElementById("startrec").classList.add("isActiveCtr");
+    // document.getElementById("stoprec").classList.remove("isActiveCtr");
+    // document.getElementById("startrec").classList.add("isActiveCtr");
     swapButtons("startrec", "stoprec");
     console.log("recorder started");
 };
@@ -156,8 +155,8 @@ function stopRec() {
     event.preventDefault();
     mediaRecorder.requestData();
     mediaRecorder.stop();
-    document.getElementById("stoprec").classList.add("isActiveCtr");
-    document.getElementById("startrec").classList.remove("isActiveCtr");
+    // document.getElementById("stoprec").classList.add("isActiveCtr");
+    // document.getElementById("startrec").classList.remove("isActiveCtr");
     swapButtons("stoprec", "startrec");
     console.log("recorder stopped");
 };
@@ -167,34 +166,25 @@ function tabActive() {
     event.preventDefault();
     row = this.getAttribute("data-row");
     col = this.getAttribute("data-col");
-    beatsMatrix[row][col] = !beatsMatrix[row][col];
-    soundLinks[row][col].classList.toggle("isActiveButt");
-}
-
-//adds event listener to each element with className
-for (let r = 0; r < numberOfInstr; r++) {
-    console.log(soundLinks);
-    soundLinks[r].forEach(element => element.addEventListener("click", tabActive));
+    musicMachine[row][col].classList.toggle("isActiveButt");
 }
 
 function playAudio() {
+    //let id = row 
     for (row = 0; row < numberOfInstr; row++) {
-        if (counter > -1) 
-            soundLinks[row][counter].classList.add("counterPos");
-    }
-    for (row = 0; row < numberOfInstr; row++) {
+        if (counter > -1) musicMachine[row][counter].classList.add("counterPos");
+        
         if (counter > 0) {
-            soundLinks[row][counter - 1].classList.remove("counterPos");
+            musicMachine[row][counter - 1].classList.remove("counterPos");
         } else {
-            soundLinks[row][numberOfBeats - 1].classList.remove("counterPos");
+            musicMachine[row][numberOfBeats - 1].classList.remove("counterPos");
         }
-    }
-    //match data key of row to data key of sound
-    for (row = 0; row < numberOfInstr; row++) {
-        if (beatsMatrix[row][counter]) { 
-           audio[row].currentTime = 0;
-           audio[row].play();
-        }
+        //beatsMatrix[row][counter]
+        if (musicMachine[row][counter].classList.contains("isActiveButt")) { 
+            console.log('true');
+            audio[row].currentTime = 0;
+            audio[row].play();
+         }
     }
     counter++;
     //loops through array
@@ -203,11 +193,19 @@ function playAudio() {
     }
 }
 
-//Event LISTENERS
-document.getElementById("start").addEventListener("click", start);
-document.getElementById("pause").addEventListener("click", pause);
-document.getElementById("stop").addEventListener("click", stop);
-document.getElementById("startrec").addEventListener("click", startRec);
-document.getElementById("stoprec").addEventListener("click", stopRec);
-document.getElementById("bpm").addEventListener("input", timeInterval);
+function main(){
+    setUp();
+    //adds event listener to each element with className
+    for (let r = 0; r < numberOfInstr; r++) {
+        musicMachine[r].forEach(element => element.addEventListener("click", tabActive));
+    }
+    //Event LISTENERS
+    document.getElementById("start").addEventListener("click", start);
+    document.getElementById("pause").addEventListener("click", pause);
+    document.getElementById("stop").addEventListener("click", stop);
+    document.getElementById("startrec").addEventListener("click", startRec);
+    document.getElementById("stoprec").addEventListener("click", stopRec);
+    document.getElementById("bpm").addEventListener("input", timeInterval);
+}
 
+main();
